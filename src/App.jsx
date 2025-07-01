c99b69d59fb4bfdee99fc5b41f3c6a4779a0067b
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-07-01 10:51:32 +0530

    Fresh start

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const [data, setData] = useState(""); 
+
+  const handleClick = async () => {
+    const input = prompt("Enter color name");
+    try {
+      const res = await axios.get(`http://localhost:5000/${input}`);
+      setData(input); 
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <>
+      <div style={{ height: "20vh", width: "20vw", backgroundColor: data }}>
+
+        <h1 style={{ color: "#fff", textAlign: "center", paddingTop: "20px" }}>
+          {data ? `Background color: ${data}` : "Click the button to set color"}
+        </h1>
+      </div>
+      <button onClick={handleClick}>Click here</button>
+    </>
+  );
+}
+
+export default App;
